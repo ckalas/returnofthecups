674131e79e038aaec6ac626f081760a6fb5e9ac9
AAAlpha <andy.tran@uqconnect.edu.au>
2014-10-15 16:55:49 +1000

    Minor update and fps counter

--- vision/cups.cpp
+++ vision/cups.cpp
@@ -8,33 +8,45 @@ using namespace std;
 
 void detect_cups(Mat *rgbMat, Mat *depthMat, CascadeClassifier cascade, Mat *inverseCamera) {
 
-	std::vector<cv::Rect> matches;
-	Mat gray, cameraCoords; // make this a vector of them ultimately
-	uint8_t *depthPtr,z = 0;
-	cvtColor(*rgbMat, gray, CV_BGR2GRAY);
-	equalizeHist(gray,gray);
+    std::vector<cv::Rect> matches;
+    Mat gray, cameraCoords; // make this a vector of them ultimately
+    uint8_t *depthPtr, z = 0;
+    cvtColor(*rgbMat, gray, CV_BGR2GRAY);
+    equalizeHist(gray,gray);
 
-	cascade.detectMultiScale(gray, matches, 1.3, 3,0|CV_HAAR_SCALE_IMAGE, Size(20, 30));
-	for (size_t i = 0; i < matches.size(); i++) {
-		// Draw rectangle
-		Point tl (matches[i].x, matches[i].y);
-		Point br (matches[i].x+matches[i].width, matches[i].y+matches[i].height);
-		rectangle(*rgbMat, tl ,br, Scalar( 0, 255, 255 ), +2, 4);
-		// Get depth of cup
-		Mat roi(*depthMat, matches[i]);
-		depthPtr =  (uint8_t*)roi.col(matches[i].width / 2).data;
-		int fix = 0;
-		while (!z) {
-			z = (uint8_t)depthPtr[matches[i].height/2 + fix] ;
-			fix += 1;
-			cout << z << endl;
-		}
-		cout << z << endl;
+    cascade.detectMultiScale(gray, matches, 1.3, 3,0|CV_HAAR_SCALE_IMAGE, Size(20, 30));
 	
-		// Calculate distance from camera
-		//Mat imageCoords = (Mat_<double>(1,3) << x*, -1, 0, -1, 5, -1, 0, -1, 0);
-		//cameraCoords = *inverseCamera * 
+    for (size_t i = 0; i < matches.size(); i++) {
+	// Draw rectangle
+	Point tl (matches[i].x, matches[i].y);
+	Point br (matches[i].x+matches[i].width, matches[i].y+matches[i].height);
+	rectangle(*rgbMat, tl ,br, Scalar( 0, 255, 255 ), +2, 4);
+	// Get depth of cup
+	Mat roi(*depthMat, matches[i]);
+	imshow("test", roi);
+	// depthPtr =  (uint8_t*)roi.col(matches[i].width / 2).data;
+	depthPtr = roi.col(matches[i].width / 2).data;
+	int fix = 0;
+	/*	
+	z = 0;
+
+
+	while (!z) {
+
+	    // cout << depthPtr[matches[i].height/2] << endl;
+	    z = 1;
+	    /*
+	    z = (uint8_t)depthPtr[matches[i].height/2 + fix] ;
+	    fix += 1;
+	    cout << z << endl;
 
 	}
+	cout << z << endl;
+	*/
+	// Calculate distance from camera
+	//Mat imageCoords = (Mat_<double>(1,3) << x*, -1, 0, -1, 5, -1, 0, -1, 0);
+	//cameraCoords = *inverseCamera * 
 
+	
+    }
 }
--- vision/main.cpp
+++ vision/main.cpp
@@ -13,61 +13,68 @@ using namespace std;
 
 int main(int argc, char **argv) {
 
-	/*
-	// Load classifier
-	CascadeClassifier rectCup;
-	if (!rectCup.load("rectCup.xml")) {
-		cout << "Error loading classifier" << endl;
+    // Variables to determine FPS
+    long int e1, e2;
+    char k;
+    double t;
+    int fps;
+
+    // Load classifier
+    CascadeClassifier rectCup;
+    if (!rectCup.load("rectCup.xml")) {
+	cout << "Error loading classifier" << endl;
+    }
+	
+    bool die(false);
+	
+    Mat depthMat(Size(640,480),CV_16UC1);
+    Mat depthf (Size(640,480),CV_8UC1);
+    Mat rgbMat(Size(640,480),CV_8UC3,Scalar(0));
+    Mat ownMat(Size(640,480),CV_8UC3,Scalar(0));
+
+    Mat cameraMatrix, dist, cameraInv;
+	
+    Freenect::Freenect freenect;
+    MyFreenectDevice &device = freenect.createDevice<MyFreenectDevice>(0);
+	
+    namedWindow("rgb",CV_WINDOW_AUTOSIZE);
+    namedWindow("depth",CV_WINDOW_AUTOSIZE);
+    device.startVideo();
+    device.startDepth();
+    device.getCameraParams(&cameraMatrix,&dist,&cameraInv);
+    while (!die) {
+	// Check the clock tick
+	//1 = 
+
+	// Get new frames
+	device.getVideo(rgbMat);
+	device.getDepth(depthMat);
+
+	//detect_cups(&rgbMat, rectCup);
+
+	cv::imshow("rgb", rgbMat);
+	cv::moveWindow("rgb", 0, 0);
+
+	depthMat.convertTo(depthf, CV_8UC1, 255.0/2048.0);
+	// Detect and locate cup/s
+	detect_cups(&rgbMat, &depthf, rectCup, &cameraInv);
+
+	cv::imshow("rgb", rgbMat);
+	cv::moveWindow("rgb", 0, 0);
+
+	cv::imshow("depth",depthf); 
+	cv::moveWindow("depth", 650, 0);
+
+	char k = cvWaitKey(1);
+
+	if( k == 'q' || k == 'Q' ){
+	    cvDestroyWindow("rgb");
+	    cvDestroyWindow("depth");
+	    break;
 	}
-	*/
-	bool die(false);
-	
-	Mat depthMat(Size(640,480),CV_16UC1);
-	Mat depthf (Size(640,480),CV_8UC1);
-	Mat rgbMat(Size(640,480),CV_8UC3,Scalar(0));
-	Mat ownMat(Size(640,480),CV_8UC3,Scalar(0));
+    }
 
-	Mat cameraMatrix, dist, cameraInv;
-	
-	Freenect::Freenect freenect;
-	MyFreenectDevice &device = freenect.createDevice<MyFreenectDevice>(0);
-	
-	namedWindow("rgb",CV_WINDOW_AUTOSIZE);
-	namedWindow("depth",CV_WINDOW_AUTOSIZE);
-	device.startVideo();
-	device.startDepth();
-	device.getCameraParams(&cameraMatrix,&dist,&cameraInv);
-	while (!die) {
-		// Get new frames
-		device.getVideo(rgbMat);
-		device.getDepth(depthMat);
-
-		//detect_cups(&rgbMat, rectCup);
-
-		cv::imshow("rgb", rgbMat);
-		cv::moveWindow("rgb", 0, 0);
-
-		depthMat.convertTo(depthf, CV_8UC1, 255.0/2048.0);
-		// Detect and locate cup/s
-		// detect_cups(&rgbMat, &depthMat, rectCup, &cameraInv);
-
-		cv::imshow("rgb", rgbMat);
-		cv::moveWindow("rgb", 0, 0);
-
-		cv::imshow("depth",depthf); 
-		cv::moveWindow("depth", 650, 0);
-
-		char k = cvWaitKey(5);
-
-		if( k == 'q' || k == 'Q' ){
-			cvDestroyWindow("rgb");
-			cvDestroyWindow("depth");
-			break;
-		}
-
-	}
-
-	device.stopVideo();
-	device.stopDepth();
-	return 0;
+    device.stopVideo();
+    device.stopDepth();
+    return 0;
 }
