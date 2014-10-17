82218aabd249f35fbbd59e293849848a3d2a209b
AAAlpha <andy.tran@uqconnect.edu.au>
2014-10-18 01:59:47 +1000

    Testing code with different motors and check if incremental position smooth out motion of robot

--- robot/dxl_hal.c
+++ robot/dxl_hal.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <termios.h>
+//#include <termio.h>
 #include <linux/serial.h>
 #include <sys/ioctl.h>
 #include <sys/time.h>
--- robot/main.cpp
+++ robot/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-#include "multi_dnmx_motor.h"
+#include "multi_motor.h"
+#include <cv.h>
+#include <highgui.h>
 
 using namespace std;
+using namespace cv;
 
 int main( int argc, char *argv[] ) 
 {
@@ -9,16 +12,35 @@ int main( int argc, char *argv[] )
 	
     CMulti_DNMX_Motor Motors;
 
-    int a = 256;
-    int goal_pos[3] = {a,a,a};//{512,512,512};
+    int a = 512;
+    int goal_pos[3] = {2048, a, a};
     int curr_pos[3] ={0,0,0};
     //int temp;
 
-    // init with baud 20k, refer to bauds.txt for mapping
+    // init with baud 20k, rgefer to bauds.txt for mapping
     Motors.initialization(34);
 
+    Motors.move_to_goal_pos(goal_pos, curr_pos);
 
-    for (int j=0; j<20; j++) {
+    char k;
+
+    /****************
+     * Need to get the motors to move at small increment to allow smooth movement
+     * and also a function to check if the motor has reach +- 5 or 10 of the goal pos
+     * vs the curr_pos. 
+     * Also you need to set an angle limit to prevent the motor from getting pass
+     * certain limit which might cause tangling of wires.
+     *
+     * The MX-12W works the current code
+     */
+
+    While10 (1) {
+	
+	cin.get();
+
+	goal_pos[0] -= 5;
+
+	//for (int j=0; j<20; j++) {
 	Motors.move_to_goal_pos(goal_pos,curr_pos);
 
 	for(int i=0;i<3;i++){
