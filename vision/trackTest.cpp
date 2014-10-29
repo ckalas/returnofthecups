388b9faeba6a14563ec9377f1332a733100744fb
kenzie0201 <kenzie_0201@hotmail.com>
2014-10-29 14:36:58 +1000

    enjoy

--- vision/trackTest.cpp
+++ vision/trackTest.cpp
@@ -1,7 +1,6 @@
-#include "opencv2/video/tracking.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/videoio/videoio.hpp"
-#include "opencv2/highgui/highgui.hpp"
+
+#include <highgui.h>
+#include <cv.h>
 
 #include <iostream>
 #include <ctype.h>
