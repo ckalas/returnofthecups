0904e68b801d772308e62cff42cb026a0f748cf9
Chris Kalas <chrisjkalas@gmail.com>
2014-11-01 12:26:51 +1000

    ;alksjdfl;asd

--- test/one.cpp
+++ test/one.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unistd.h>
+ #include <stdlib.h>
 #include <fstream>
 #include <vector>
 
@@ -7,7 +8,27 @@ using namespace std;
 
 int main() {
     int x;
-    cout << 3 << endl << 5 << endl << 100 << endl << 1<< endl;
+    int to[2], from[2];
+    pipe(to);
+    pipe(from);
+
+    // childs perspective to/from
+    if (fork()) { // parent
+        close(to[0]);
+        close(from[1]);
+        dup2(to[1], 1);
+        dup2(from[0], 0);
+    }
+    else {
+        close(to[1]);
+        close(from[0]);
+        dup2(to[0], 0);
+        dup2(from[1], 1);
+        close(to[0]);
+        close(from[1]);
+        execvp("two", NULL);
+    }
+    cout << 32 << endl << 54 << endl << 100 << endl << 1<< endl;
     cout.flush();
     cin >>  x; 
     cerr <<  "received " << x << endl;
