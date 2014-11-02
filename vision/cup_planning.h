51c21799f31009411f5b7393ea30321a776139b4
kenzie0201 <kenzie_0201@hotmail.com>
2014-11-02 13:37:51 +1000

    cup planning

---
+++ vision/cup_planning.h
@@ -0,0 +1,11 @@
+#ifndef CUP_PLANNING_H
+#define CUP_PLANNING_H
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void cup_prediction(float t, Point2f p_0, Point2f* p_c);
+
+#endif //CUP_PLANNING_H
