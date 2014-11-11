ff2999f0261fbca2b8955fb451498f403d1e0cfd
Wilson Kong <w.k.wilson-kong@hotmail.com>
2014-11-11 17:36:18 +1000

    cup planning andy's bad

--- vision/cup_planning.cpp
+++ vision/cup_planning.cpp
@@ -1,4 +1,5 @@
 #include "cup_planning.h"
+#include <cv.h>
 
 #define _USE_MATH_DEFINES 
 
@@ -6,10 +7,20 @@
 //x_init - initial x distance from base to cup  
 //y_init - initial y distance from base to cup
 void cup_prediction(float t, Point2f p_0, Point2f* p_c) {
-    float y_t = 0; //distance between arm and centre of table
+    //float y_t = 0; //distance between arm and centre of table
     float RPM = 2;
 
+    //offset the coord with table at the center
+    Point2f offSet; //offset from fiducial to center of turn table
+    offSet.x = 0;
+    offSet.y = 0;
+    Point2f pointOnTable;
+    pointOnTable.x = p_0.x + offSet.x;
+    pointOnTable.y = p_0.y + offSet.y;
+
     //theta = 0 corresponds to the +x_axis in rectangular coords
-    *p_c.x = sqrt(pow(p_0.x, 2) + pow(p_0.y, 2)) * cos(atan(p_0.y / p_0.x) + (M_PI / 30) * t * RPM);
-    *p_c.y = sqrt(pow(p_0.x, 2) + pow(p_0.y, 2)) * sin(atan(p_0.y / p_0.x) + (M_PI / 30) * t * RPM) + y_t;
+	*p_c.x = (sqrt(pow(pointOnTable.x, 2) + pow(pointOnTable.y, 2)) * 
+		cos(atan(pointOnTable.y / pointOnTable.x) + (M_PI / 30) * t * RPM)) - offSet.x;
+    *p_c.y = sqrt(pow(pointOnTable.x, 2) + pow(pointOnTable.y, 2)) * 
+    	sin(atan(pointOnTable.y / pointOnTable.x) + (M_PI / 30) * t * RPM) - offSet.y;
 }
